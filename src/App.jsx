632f9ad385041a9a76d57563967eca1183455aa1
JeanFelixHAKIZIMANA <hakfelix90gmail.com>
2024-06-24 17:36:56 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { Hero,
+  Footer,
+  CustomerReview,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality 
+} from './sections'
+import  Nav  from './components/Nav'
+
+const App = () => (
+  <main className="relative">
+    <Nav /> 
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+
+    <section className="padding">
+      <PopularProducts />
+    </section>
+
+    <section className="padding">
+      <SuperQuality />
+    </section>
+
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+
+    <section className="padding">
+      <SpecialOffers />
+    </section>
+
+    <section className="padding bg-pale-blue">
+      <CustomerReview />
+    </section>
+
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    
+    <section className="padding-x bg-black padding-t text-white pb-8">
+      <Footer />
+    </section>
+  </main>
+)
+
+export default App; 
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <section 
+    id='home'
+    className="border-2 border-red-500 flex p-2 xl:flex-row flex-col justify-center min-h-screen w-full ç z-1"
+    >
+
+    </section>
+  )
+}
+
+export default Hero
